b04fb6d794c3062c0df4dec09148cc32afba1840
Fedor <fedor.sannikov88@gmail.com>
2025-09-25 00:50:46 +0700

    fix bug

---
+++ exercises with metanit.com/variables and data types/exercise2.c
@@ -0,0 +1,19 @@
+/*
+Exercise 2
+Write a program in which you define four variables of types char, int, float and double
+
+Упражнение 2
+Напишите программу, в которой определите четыре переменных типов char, int, float и double
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    
+    printf("Hello world!\nHello C!\n");
+
+    return 0;
+}
+
+//gcc exercise1.c -o exercise1 && ./exercise1
